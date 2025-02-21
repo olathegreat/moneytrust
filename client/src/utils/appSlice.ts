0982c95ae49872b8565afffa91fcd495ebd4e5e8
olathegreat <ademolarotimi2000@gmail.com>
2025-02-21 12:20:16 +0100

    feat

--- client/src/utils/appSlice.ts
+++ client/src/utils/appSlice.ts
@@ -1,54 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { UserInfoType } from "../pages/DashboardPage";
 
-export type messageType = {
-    _id?: string;
-    sender: string;
-    recipient: string;
-    messageType: string;
-    content?: string;
-    fileUrl?: string;
-    timeStamp?: Date;
-}
+
 
 interface AppState {
 
-    formDisplay: "signup" | "signin";
-    userDetails: UserInfoType;
-    selectedUser: UserInfoType;
-    messagesArray: messageType[] | [];
-    isFileUpload: boolean;
-    fileUploadProgress: number;
-    isFileDownload: boolean;
-    fileDownloadProgress: number;
-    directMessagesContact: UserInfoType[] | [];
-    mobileViewChatDisplay: boolean
+ mobileViewChatDisplay: boolean;
+   
 }
 
 const initialState: AppState = {
 
-    formDisplay: "signup",
-    userDetails: {
-        fullname: "",
-        email: "",
-        picture: "",
-        _id: "",
-        about: "",
 
-    },
-    selectedUser: {
-        fullname: "",
-        email: "",
-        picture: "",
-        _id: "",
-        about: ""
-    },
-    messagesArray: [],
-    isFileUpload:false,
-    isFileDownload: false,
-    fileDownloadProgress:0,
-    fileUploadProgress:0,
-    directMessagesContact:[],
+    
     mobileViewChatDisplay: false
 };
 
@@ -56,46 +19,14 @@ const appSlice = createSlice({
     name: "app",
     initialState,
     reducers: {
-        setFormDisplay: (state, action) => {
-            state.formDisplay = action.payload
-        },
-        setUserDetails: (state, action) => {
-            state.userDetails = action.payload
-        },
-        setSelectedUser: (state, action) => {
-            state.selectedUser = action.payload
-            console.log(state.selectedUser);
-        },
-        addUserMessages: (state, action)=>{
-            state.messagesArray = [...state.messagesArray, action.payload
-            ]
-        },
-        setUserMessages: (state, action)=>{
-            state.messagesArray = action.payload
-        },
-        setIsFileUpload:(state)=>{
-            state.isFileUpload = !state.isFileUpload
-        },
-        setIsFileDownload:(state)=>{
-            state.isFileDownload = !state.isFileDownload
-        },
-        setFileUploadProgress:(state, action)=>{
-            state.fileUploadProgress = action.payload
-        },
-        setFileDownloadProgress:(state, action)=>{
-            state.fileDownloadProgress = action.payload
-        },
-        setDirectMessagesContact: (state, action) => {
-            state.directMessagesContact = action.payload
-
-
-        },
+    
+       
         setMobileViewChatDisplay: (state)=>{
             state.mobileViewChatDisplay = !state.mobileViewChatDisplay;
         }
     }
 })
 
-export const { setDirectMessagesContact,setFileDownloadProgress,setFileUploadProgress,setIsFileDownload, setIsFileUpload,setFormDisplay,addUserMessages,setUserMessages, setUserDetails, setSelectedUser, setMobileViewChatDisplay } = appSlice.actions;
+export const {  setMobileViewChatDisplay } = appSlice.actions;
 
 export default appSlice.reducer;
